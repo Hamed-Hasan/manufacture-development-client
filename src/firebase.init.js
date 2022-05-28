33b842b9e371f5f5ef65e08b3206be96a7f7883f
John Doe <hamedhasan422421@gmail.com>
2022-05-28 14:34:24 +0300

    update client

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,18 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // manufacture-development project
 const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_API_KEY,
-  // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  // projectId: process.env.REACT_APP_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  // appId: process.env.REACT_APP_APP_ID,
-  apiKey: "AIzaSyADYFCIKr-0-yYMpmuv176oyZ8U8Rm_plk",
-  authDomain: "manufacture-development.firebaseapp.com",
-  projectId: "manufacture-development",
-  storageBucket: "manufacture-development.appspot.com",
-  messagingSenderId: "304352925661",
-  appId: "1:304352925661:web:47b850d3b657909f060834"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
