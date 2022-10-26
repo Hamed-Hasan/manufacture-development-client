81163b075cebd90e9d8ac4657931f9c1188f848e
John Doe <hamedhasan422421@gmail.com>
2022-10-26 03:42:47 +0300

    optimize react query

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import {
-  QueryClient,
-  QueryClientProvider,
-} from 'react-query'
 
+const { QueryClient, QueryClientProvider, } = lazy(() => import('react-query'));
 const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
